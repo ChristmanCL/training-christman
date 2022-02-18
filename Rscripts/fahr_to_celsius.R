ef86bc0a66127097a9963d959654d29f1d946011
Cynthia Christman <cynthia.christman@noaa.gov>
2022-02-17 16:57:43 -0800

    Updates

---
+++ Rscripts/fahr_to_celsius.R
@@ -0,0 +1,32 @@
+#' Fahrenheit to Celsius
+#' 
+#' @param fahr Temperature in Fahrenheit as a vector
+#'
+#' @return Temperature in Celsius
+#' @export 
+#'
+#' @examples
+#' fahr_to_celsius(c(212, 32))
+#' 
+
+fahr_to_celsius <- function(fahr){
+  celsius <- (fahr - 32)*5/9
+  return(celsius)
+}
+
+celsius_to_fahr <- function(celsius){
+  fahr <- celsius*9/5 + 32
+  return(fahr)
+}
+
+
+convert_temps <- function(fahr){
+  celsius <- (fahr - 32)*5/9
+  kelvin <- celsius + 273.15
+  return(data.frame(fahrenheit = fahr, 
+              celsius = celsius, 
+              kelvin = kelvin))
+  }
+
+convert_temps(airtemps)
+
